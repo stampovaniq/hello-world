163758cf3f699e263e6921d7df0954471ef44e41
stampovaniq <ilkovn@abv.bg>
2016-04-01 00:35:56 +0300

    Added files via upload

---
+++ MatrixNumbers/MatrixNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MatrixNumbers
+{
+    static void Main(string[] args)
+    {
+        int n;
+        Console.Write("Enter positive number: ");
+        n = int.Parse(Console.ReadLine());
+        int temp;
+        for (int i = 1; i <= n; i++)
+        {
+            temp = i;
+            for (int j = 1; j <= n; j++)
+            {
+                Console.Write(temp + " ");
+                temp++;
+            }
+            Console.WriteLine("");
+        }
+    }
+}
